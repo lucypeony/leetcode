9b157e33991f6afef5c0826191e933188eeb65a0
lucypeony <lucypeony@hotmail.com>
2018-05-28 00:42:50 +0800

    Update reverse integer.cpp

--- reverse integer.cpp
+++ reverse integer.cpp
@@ -1,3 +1,13 @@
+/*
+lucypeony@hotmail.com
+
+leetcode: Reverse Integer 
+the difficulty lies how to detect the integer overflow. 
+if integer overflows , output 0
+
+I use an ad hoc method to solve this problem.
+*/
+
 class Solution {
 public:
 	int reverse(int x) {
@@ -8,15 +18,16 @@ public:
 			x = 0 - x;
 		}
 
-		int y = 0;
+		long long  y = 0;
 		while (x > 0) {
 			int t = x % 10;
-			if (y > LONG_MAX / 10)
-				return 0;
 			y *= 10;
 			y += t;
 			x /= 10;
 		}
+
+		if (y > LONG_MAX)
+			return 0;
 
 		if (f)
 			y = 0 - y;
